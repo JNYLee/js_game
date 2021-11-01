31e81432b9ea819adec9c8c8e21545dedd79b13c
JNYLee <junyjuny1021@gmail.com>
2021-11-01 23:47:12 +0900

    [브런치 공부]: 브런치에 새로운파일 저장

--- hello.js
+++ hello.js
@@ -55,22 +55,3 @@ while (i < 10) {
     console.log(i);
 }
 
-for(let i = 1; i < 10; i++){
-    for(let j = 1; j < 10; j++) {
-        if(i % 2 == 0 || j % 2 == 0) continue;
-        console.log(i, "X", j);
-    }
-
-}
-for(let starNum = 0; starNum < 5; starNum++)
-{
-    console.log(" ".repeat(4-starNum),"*".repeat(starNum+1));
-}
-
-//'라'를 모두 제거 하자
-const arr = ['가', '라', '마', '라', '바', '라'];
-for(let indexNum = 0; indexNum < arr.length; i++)
-{
-    if(arr[indexNum] == '라') arr.splice(indexNum, 1);
-}
-console.log(arr);

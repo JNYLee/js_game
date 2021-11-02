7aff05c6299d2a8a22ce7fdaed18ffdebf1b83b0
JNYLee <junyjuny1021@gmail.com>
2021-11-02 18:36:10 +0900

    [자바스크립트공부]: 5-8까지 forEach, map, fill

--- hello.js
+++ hello.js
@@ -1,57 +1,80 @@
-const hello = "엄청나게 어려운 코드"; /* 사람끼리 의사소통해요. 설명 적어줄게요. */
-// 이것도 주석
-const money = '1000,000';
-//const money = '1000,000';
-if (condition) {
-    const money1 = '1000,000';
-}
 
-const SCORE = 75;
- if (score >= 90) {
-     console.log('A++');
- }else if (score >= 80){
-    console.log('A+');
- }else if (score >= 70){
-    console.log('B');
- } else {
-    console.log("F");
-}
-let value = 'B';
-switch (value){
-    case 'A':
-        console.log('A');
-        break;
-    case 'B':
-        console.log('B');
-        break;
-    case 'C':
-        console.log('C');
-        break;
-}
+    //const hello = "엄청나게 어려운 코드"; /* 사람끼리 의사소통해요. 설명 적어줄게요. */
+    // 이것도 주석
+   // const money = '1000,000';
+    //const money = '1000,000';
+        /*if (condition) {
+            const money1 = '1000,000';
+        }
 
-let i = 1;
-while(i <= 100) {
-    console.log("Hello, While!");
-    i++;
-}
+        const SCORE = 75;
+        if (score >= 90) {
+            console.log('A++');
+        }else if (score >= 80){
+            console.log('A+');
+        }else if (score >= 70){
+            console.log('B');
+        } else {
+            console.log("F");
+        }
+        let value = 'B';
+        switch (value){
+            case 'A':
+                console.log('A');
+                break;
+            case 'B':
+                console.log('B');
+                break;
+            case 'C':
+                console.log('C');
+                break;
+        }
 
-for (let i = 0; i <=99; i++){
-    console.log(i+1);
-}
+        let i = 1;
+        while(i <= 100) {
+            console.log("Hello, While!");
+            i++;
+        }
 
-let i =0;
-while (true) {
-    if(i===5) break;
-        i++;
-}
-console.log(i);
+        for (let i = 0; i <=99; i++){
+            console.log(i+1);
+        }
 
-let i = 0;
-while (i < 10) {
-    i++;
-    if(i % 2 === 0) { //짝수는 건너뛰기
-        continue;
-    }
-    console.log(i);
-}
+        let i =0;
+        while (true) {
+            if(i===5) break;
+                i++;
+        }
+        console.log(i);
 
+        let i = 0;
+        while (i < 10) {
+            i++;
+            if(i % 2 === 0) { //짝수는 건너뛰기
+                continue;
+            }
+            console.log(i);
+        }
+    */
+/* function hello() {
+    hello.a = 'really';
+    const array = [];
+    array.b = 'wow';
+    console.log(hello.a);
+    console.log(array.b);
+}
+*/
+// const zerocho = {
+//     name: {
+//         first:'현영',
+//         last: '조',
+//     },
+//     gender: 'm',
+// };
+// console.log(zerocho.name.last);
+
+let firstName = null;
+let middleName = "La";
+let finalName = "Tiridi";
+
+alert(firstName ?? middleName ?? finalName ?? "unKnownName");

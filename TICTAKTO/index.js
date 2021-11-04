9fc581627a3e890efef18cc9055a7f35d5a261e6
JNYLee <junyjuny1021@gmail.com>
2021-11-04 18:57:35 +0900

    [tictakto\init]: 이차원 배열 다루기

--- TICTAKTO/index.js
+++ TICTAKTO/index.js
@@ -1,9 +1,28 @@
 document.addEventListener("DOMContentLoaded", ()=> {
     const data = [];
     const horizon_blank = 3;
-    for(let make_horizon_blank = 0; make_horizon_blank < horizon_blank; make_horizon_blank++) data.push([]);
-
+    let turn = 'O';
+    for(let make_horizon_blank = 0; make_horizon_blank < horizon_blank; make_horizon_blank++) {
+      data.push([]);
+    }
     const $table = document.createElement('table');
+    for(let make_blank_tr = 0; make_blank_tr < horizon_blank; make_blank_tr++) {
+      const $tr = document.createElement('tr'); 
+      for(let make_blank_td = 0; make_blank_td < horizon_blank; make_blank_td++) {
+        const $td = document.createElement('td'); 
+        $td.addEventListener('click', (event) => {
+          if (event.target.textContent) return;
+          event.target.textContent = turn;
+          if (turn === 'O') {
+            turn = 'X';
+          }else if (turn === 'X') {
+            turn = 'O';
+          }
+        });
+        $tr.append($td);
+      }
+      $table.append($tr);
+    }
 
     // 테이블의 구조 참고  <table>
     // <tr>

19a0ef4f254ed768849656b963399a8a54b93d9d
JNYLee <junyjuny1021@gmail.com>
2021-11-04 19:02:34 +0900

    [tictakto\init]:표 다시 그리기 1/3

--- TICTAKTO/index.js
+++ TICTAKTO/index.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", ()=> {
+    
     const data = [];
     const horizon_blank = 3;
+    const $table = document.createElement('table');
+    const $result = document.createElement('div');
+    
     let turn = 'O';
+
     for(let make_horizon_blank = 0; make_horizon_blank < horizon_blank; make_horizon_blank++) {
       data.push([]);
     }
-    const $table = document.createElement('table');
     for(let make_blank_tr = 0; make_blank_tr < horizon_blank; make_blank_tr++) {
       const $tr = document.createElement('tr'); 
       for(let make_blank_td = 0; make_blank_td < horizon_blank; make_blank_td++) {
@@ -42,7 +46,8 @@ document.addEventListener("DOMContentLoaded", ()=> {
     // </tr>
     // </table> -->
 
-    document.body.append($table);
-
+  document.body.append($table);
+  document.body.append($result);
+    
 
 });

d8545280176f58d7c9bde26e660418f92040f451
JNYLee <junyjuny1021@gmail.com>
2021-11-04 21:54:37 +0900

    [tictakto\init]:표 다시 그리기

--- TICTAKTO/index.js
+++ TICTAKTO/index.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded", ()=> {
     
+    const { body} = document;
     const data = [];
     const horizon_blank = 3;
     const $table = document.createElement('table');
     const $result = document.createElement('div');
-    
+    const rows = [];
     let turn = 'O';
 
     for(let make_horizon_blank = 0; make_horizon_blank < horizon_blank; make_horizon_blank++) {
       data.push([]);
     }
     for(let make_blank_tr = 0; make_blank_tr < horizon_blank; make_blank_tr++) {
-      const $tr = document.createElement('tr'); 
+      const $tr = document.createElement('tr');
+      const cells = [];
       for(let make_blank_td = 0; make_blank_td < horizon_blank; make_blank_td++) {
         const $td = document.createElement('td'); 
+        cells.push($td);
         $td.addEventListener('click', (event) => {
           if (event.target.textContent) return;
           event.target.textContent = turn;
@@ -46,8 +49,8 @@ document.addEventListener("DOMContentLoaded", ()=> {
     // </tr>
     // </table> -->
 
-  document.body.append($table);
-  document.body.append($result);
+  body.append($table);
+  body.append($result);
     
 
 });

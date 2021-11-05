53c8f78aae4c08010b853b615cd77f85e4972712
JNYLee <junyjuny1021@gmail.com>
2021-11-05 17:48:15 +0900

    [cardMatchGame]: 이벤트 루프 분석으로 버그 해결하기

--- cardMatchGame/index.js
+++ cardMatchGame/index.js
@@ -1,29 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const $wrapper = document.querySelector('#wrapper');
+  const $wrapper = document.querySelector("#wrapper");
   const total = 12;
-  const colors = ['red', 'orange', 'yellow', 'green', 'white', 'pink'];
+  const colors = ["red", "orange", "yellow", "green", "white", "pink"];
   let colorCopy = colors.concat(colors);
   let shuffled = [];
   let clicked = [];
   let completed = [];
   let clickable = false;
 
-  function shuffle() { // 색깔들을 무작위로 섞어주는 함수
-    for(let i = 0; colorCopy.length > 0; i += 1) {
+  function shuffle() {
+    // 색깔들을 무작위로 섞어주는 함수
+    for (let i = 0; colorCopy.length > 0; i += 1) {
       const randomIndex = Math.floor(Math.random() * colorCopy.length);
       shuffled = shuffled.concat(colorCopy.splice(randomIndex, 1));
     }
   }
 
-  function createCard(i) { // 카드를 만드는 함수
-    const card = document.createElement('div');
-    card.className = 'card';
-    const cardInner = document.createElement('div');
-    cardInner.className = 'card-inner';
-    const cardFront = document.createElement('div');
-    cardFront.className = 'card-front';
-    const cardBack = document.createElement('div');
-    cardBack.className = 'card-back';
+  function createCard(i) {
+    // 카드를 만드는 함수
+    const card = document.createElement("div");
+    card.className = "card";
+    const cardInner = document.createElement("div");
+    cardInner.className = "card-inner";
+    const cardFront = document.createElement("div");
+    cardFront.className = "card-front";
+    const cardBack = document.createElement("div");
+    cardBack.className = "card-back";
     cardBack.style.backgroundColor = shuffled[i]; // 카드의 색깔을 넣어주기
     cardInner.appendChild(cardFront);
     cardInner.appendChild(cardBack);
@@ -31,46 +33,51 @@ document.addEventListener("DOMContentLoaded", () => {
     return card;
   }
 
-  function onClickCard() {  //카드가 클릭했을 때 발생하는 함수? 모르겠다
-    if(!clickable || completed.includes(this) || clicked[0] === this)  return;
-     
-    this.classList.toggle('flipped');
+  function onClickCard() {
+    //카드가 클릭했을 때 발생하는 함수?
+    if (!clickable || completed.includes(this) || clicked[0] === this) return;
+
+    this.classList.toggle("flipped");
     clicked.push(this);
 
-    if (clicked.length !== 2) {return;} // 두 카드가 같은 지 확인하는 부분
-    
-    const firstBackColor = clicked[0].querySelector('.card-back').style.backgroundColor;
-    const secondBackColor = clicked[1].querySelector('.card-back').style.backgroundColor;
+    if (clicked.length !== 2) {
+      return;
+    } // 두 카드가 같은 지 확인하는 부분
+
+    const firstBackColor =
+      clicked[0].querySelector(".card-back").style.backgroundColor;
+    const secondBackColor =
+      clicked[1].querySelector(".card-back").style.backgroundColor;
     if (firstBackColor === secondBackColor) {
       clickable = false;
-      completed.push(clicked[0]);
-      completed.push(clicked[1]);
-      clicked = [];
-      clickable = true;
+     // completed.push(clicked[0]);
+     // completed.push(clicked[1]);
+      setTimeout(() => {
+        clicked[0].classList.remove('flipped');
+        clicked[1].classList.remove('flipped');
+        clicked = [];
+        clickable = true;
+      }, 500);
       if (completed.length !== total) {
+        //다 뒤집어 졌는지
         return;
       }
       setTimeout(() => {
+        //게임의 엔딩 조건이 완성되면
         alert("축하합니다!");
         resetGame();
       }, 300);
       return;
     }
-    
-    setTimeout(() => {
-      clicked[0].classList.remove('flipped');
-      clicked[1].classList.remove('flipped');
-      clicked = [];
-    }, 500);
-
   }
 
   function startGame() {
     clickable = false;
     shuffle();
-    for(let i = 0; i < total; i += 1) { // 카드 수 만큼 돌리기
+    for (let i = 0; i < total; i += 1) {
+      // 카드 수 만큼 돌리기
       const card = createCard(i);
-      card.addEventListener('click', onClickCard);
+      card.addEventListener("click", onClickCard);
       $wrapper.appendChild(card);
     }
     document.querySelectorAll('.card').forEach((card, index) => {
@@ -85,7 +92,6 @@ document.addEventListener("DOMContentLoaded", () => {
       });
       clickable = true;
     }, 5000);
-
   }
 
   startGame();
@@ -97,4 +103,5 @@ document.addEventListener("DOMContentLoaded", () => {
     completed = [];
     startGame();
   }
+
 });

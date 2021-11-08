3334367661d9059f532eeb7a74aa027c41b7ba18
JNYLee <junyjuny1021@gmail.com>
2021-11-08 19:00:44 +0900

    [rsp/bug_fix]: 가위바위보 결과 출력부분 생성

--- RSP/index.js
+++ RSP/index.js
@@ -1,45 +1,104 @@
+const $computer = document.querySelector("#computer");
+const $score = document.querySelector("#score");
+const $rock = document.querySelector("#rock");
+const $scissors = document.querySelector("#scissors");
+const $paper = document.querySelector("#paper");
+const IMG_URL = "./rsp.png";
+
+let myScore = 0;
+let comScore = 0;
+
 const RSP_AIS_X = {
-    scissors: '0', // 가위
-    rock: '-220px', // 바위
-    paper: '-440px', // 보
+  scissors: "0", // 가위
+  rock: "-220px", // 바위
+  paper: "-440px", // 보
 };
 
-document.addEventListener("DOMContentLoaded",() => {
-    const $computer = document.querySelector('#computer');
-  const $score = document.querySelector('#score');
-  const $rock = document.querySelector('#rock');
-  const $scissors = document.querySelector('#scissors');
-  const $paper = document.querySelector('#paper');
-  const IMG_URL = './rsp.png';
-  $computer.style.background = `url(${IMG_URL}) -464px 0`;
-  $computer.style.backgroundSize = 'auto 200px';
-  const rspX = {
-      scissors: '0', // 가위
-      rock: '-220px', // 바위
-      paper: '-440px', // 보
-    };
-    let computerChoice = 'scissors';
-    const changeComputerHand = () => {
-        if(computerChoice === 'scissors'){
-            computerChoice = 'rock';
-        }else if (computerChoice === 'rock') {
-            computerChoice = 'paper';
-        }else if(computerChoice === 'paper') {
-            computerChoice = 'scissors';
-        }
-        //rspx.computerChoice는 안됨. 이뜻은 rspX.["computerChoice"] 인데 이 값은 이미 가위바위보 3개만 지정되어있음.
-        $computer.style.background = `url(${IMG_URL}) ${rspX[computerChoice]} 0`;
-        $computer.style.backgroundSize = `auto 200px`;
-    }
-    let intervalId = setInterval(changeComputerHand, 50);
+const computerChangeChoice = {
+    scissors: 'rock',
+    rock: 'paper',
+    paper: 'scissors'
+};
 
-    const clickButton = () => {
-        clearInterval(intervalId);
+function checkMatchResult(diffChoiceKey) {
+    if ([2, -1].includes(diffChoiceKey)) {
+        myScore += 1;
+        return '승리';
+      } else if ([-2, 1].includes(diffChoiceKey)) {
+        comScore += 1;
+        return '패배';
+      } else {
+        return '무승부';
+      }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  $computer.style.background = `url(${IMG_URL}) -464px 0`;
+  $computer.style.backgroundSize = "auto 200px";
+  
+  let computerChoice = "scissors";
+  
+  const changeComputerHand = () => {
+    computerChoice = computerChangeChoice[computerChoice];
+        // test print computer change choice
+        // console.log(computerFirstChoice);
+    //RSP_AIS_X.computerChoice는 안됨. 이뜻은 RSP_AIS_X.["computerChoice"] 인데 이 값은 이미 가위바위보 3개만 지정되어있음.
+    $computer.style.background = `url(${IMG_URL}) ${RSP_AIS_X[computerChoice]} 0`;
+    $computer.style.backgroundSize = `auto 200px`;
+  };
+  
+  //0.05초마다 사진 바꿔주기
+  let intervalId = setInterval(changeComputerHand, 50);
+
+  const scoreTable = {
+    rock: 0,
+    scissors: 1,
+    paper: -1,
+  };
+
+  // clickButton 5번 호출, 인터벌 1번, 2번, 3번, 4번, 5번(얘만 intervalId)
+  //  그 다음에 버튼을 클릭하면 5번만 취소
+  let clickable = true;
+  let computer = 0;
+  let me = 0;
+
+  const clickButton = (event) => {
+    if (clickable) {
+      clearInterval(intervalId);
+      clickable = false;
+      // 점수 계산 및 화면 표시
+      const myChoice = event.target.textContent === '바위' 
+        ? 'rock' 
+        : event.target.textContent === '가위' 
+          ? 'scissors' 
+          : 'paper';
+      
+      const myChoiceKey = scoreTable[myChoice];
+      const computerChoiceKey = scoreTable[computerChoice];
+      const diff = myChoiceKey - computerChoiceKey;
+
+      let message;
+      // 2, -1은 승리조건이고, -2, 1은 패배조건, 점수표 참고
+      message = checkMatchResult(diff);
+      
+
+      if (myScore === 3) {
+        $score.textContent = `나의 승리 ${myScore}:${comScore}`;
+      } else if (computer === 3) {
+        $score.textContent = `컴퓨터의 승리 ${myScore}:${comScore}`;
+      } else {
+        $score.textContent = `${message} ${myScore}:${comScore}`;
+
         setTimeout(() => {
-            intervalId = setTimeout(changeComputerHand, 50);
+          clickable = true;
+          intervalId = setInterval(changeComputerHand, 50);
         }, 1000);
-    };
-    $rock.addEventListener('click', clickButton);
-    $scissors.addEventListener('click', clickButton);
-    $paper.addEventListener('click', clickButton);
+      }
+    } // if-clickable true 조건문 종료
+  }; // clickButton-event 종료
+
+  $rock.addEventListener('click', clickButton);
+  $scissors.addEventListener('click', clickButton);
+  $paper.addEventListener('click', clickButton);
 });
--- TICTAKTO/index.js
+++ TICTAKTO/index.js
@@ -1,17 +1,23 @@
 document.addEventListener("DOMContentLoaded", ()=> {
-    
+  
   const { body } = document;
   const $table = document.createElement('table');
   const $result = document.createElement('div'); // 결과창
   const rows = [];
+
   let turn = 'O';
   
   const checkWinner = (target) => {
+    
     if(!clickable) return;
+    
+    // 가로, 세로 줄 index 가져오기
     const rowIndex = target.parentNode.rowIndex;
     const cellIndex = target.cellIndex;
+
     // 세칸 다 채워졌나?
     let hasWinner = false;
+    
     // 가로줄 검사
     if (
       rows[rowIndex][0].textContent === turn &&
@@ -20,72 +26,87 @@ document.addEventListener("DOMContentLoaded", ()=> {
     ) {
       hasWinner = true;
     }
+    rows[rowIndex].every().forEach(col => col === turn)
     // 세로줄 검사
     if (
       rows[0][cellIndex].textContent === turn &&
       rows[1][cellIndex].textContent === turn &&
       rows[2][cellIndex].textContent === turn
-    ) {
-      hasWinner = true;
-    }
-    // 대각선 검사
-    if (
-      rows[0][0].textContent === turn &&
-      rows[1][1].textContent === turn &&
-      rows[2][2].textContent === turn
-    ) {
-      hasWinner = true;
-    }
+      ) {
+        hasWinner = true;
+      }
+      // 대각선 검사
+      if (
+        rows[0][0].textContent === turn &&
+        rows[1][1].textContent === turn &&
+        rows[2][2].textContent === turn
+        ) {
+          hasWinner = true;
+        }
     if (
       rows[0][2].textContent === turn &&
       rows[1][1].textContent === turn &&
       rows[2][0].textContent === turn
-    ) {
-      hasWinner = true;
-    }
+      ) {
+        hasWinner = true;
+      }
     return hasWinner;
   };
+  
   const checkWinnerAndDraw = (target) => {
+
     const hasWinner = checkWinner(target);
     // 승자가 있으면
     if (hasWinner) {
       $result.textContent = `${turn}님이 승리!`;
-      $table.removeEventListener('click', callback);
+      $table.removeEventListener('click', ifButtonClick);
       return;
     }
-    // 승자가 없으면
+    
+    // 승자가 없으면 무승부 확인
     const draw = rows.flat().every((cell) => cell.textContent);
     if (draw) {
       $result.textContent = `무승부`;
       return;
     }
+    
+    //턴 바꿔주기
     turn = turn === 'X' ? 'O' : 'X';
   };
+  
+  
   let clickable = true;
-  const callback = (event) => {
+  const ifButtonClick = (event) => {
+
+    // 연타를 못하게 막아주는 조건
     if (!clickable) {
       return;
     }
-    if (event.target.textContent !== '') { // 칸이 이미 채워져 있는가?
-      console.log('빈 칸이 아닙니다.');
+    
+    // 빈 칸 아닌지 확인
+    if (event.target.textContent !== '') {
+      alert('빈 칸이 아닙니다.');
       return;
     }
+
     // 빈 칸이면
     console.log('빈 칸입니다');
     event.target.textContent = turn;
     checkWinnerAndDraw(event.target);
-    if (turn === 'X') {
-      const emptyCells = rows.flat().filter((v) => !v.textContent);
-      const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
-      clickable = false;
+    if (turn === 'X') { // 컴퓨터 턴 일때,
+      const emptyCells = rows.flat().filter((v) => !v.textContent); // 비어있는 셀만 가져오기
+      const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]; // 비어있는 셀 중 하나 선택
+      clickable = false; // 클릭 방지
       setTimeout(() => {
         randomCell.textContent = 'X';
-        checkWinnerAndDraw(randomCell);
         clickable = true;
+        checkWinnerAndDraw(randomCell);
+        console.log(rows);
       }, 1000);
     }
   };
   
+  // 3 x 3 칸을 만듦
   for (let i = 1; i <= 3; i++) {
     const $tr = document.createElement('tr');
     const cells = [];
@@ -93,11 +114,13 @@ document.addEventListener("DOMContentLoaded", ()=> {
       const $td = document.createElement('td');
       cells.push($td);
       $tr.appendChild($td);
+     // console.log($td);
     }
     rows.push(cells);
     $table.appendChild($tr);
-    $table.addEventListener('click', callback);
+    $table.addEventListener('click', ifButtonClick);
   }
+
   body.appendChild($table);
   body.appendChild($result);
 
